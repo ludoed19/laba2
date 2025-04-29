e89942851cd629d4b8aa5876d0c88102c9f8d6f7
ludoed19 <temnovinc06@gmail.com>
2025-04-29 14:59:13 +0400

    Update лаба2.py

--- лаба2/лаба2.py
+++ лаба2/лаба2.py
@@ -1,8 +1,8 @@
 import re
-def num_to_words(digits):
+def ntw(digits):
     words = ["ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь"]
     return " ".join(words[int(d)] for d in sorted(digits))
-K = 3 
+K = 10 
 try:
     with open("input.txt", "r", encoding="utf-8") as f:
         data = f.read().strip()  
@@ -10,14 +10,18 @@ except FileNotFoundError:
     print("Ошибка: Файл 'input.txt' не найден.")
     input("Нажмите Enter для выхода...")
     exit()
-matches = re.findall(r"\b[0-7]+\b", data) 
-print("Найденные восьмеричные числа:", matches) 
-nums = [(int(m, 8), set(m)) for m in matches if len(set(m)) >= K] 
-nums = [(n, d) for n, d in nums if n <= 2047 and n % 2 == 1]  
+allowed = []
+for i in range(2048):
+    o = oct(i)[2:]
+    if i % 2 == 1 and len(set(0)) >= K:
+pattern = r'\b(?=(?:' + '|'.join(map(re.escape, allowed)) + r')\b'
+matches = re.findall(pattern, data)
+nums = [(int(m, 8), set(m)) for m in matches]  
+print("Найденные восьмеричные числа:", matches)  
 if nums:
     print("Числа:", [n[0] for n in nums])
     print("Количество:", len(nums))
-    print("Используемые цифры:", num_to_words(set.union(*[n[1] for n in nums])))
+    print("Используемые цифры:", ntw(set.union(*[n[1] for n in nums])))
 else:
     print("Нет подходящих чисел.")
 input()
