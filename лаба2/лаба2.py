0d0e78cbbe37048670e969b8bc6f60a68fb715dc
ludoed19 <temnovinc06@gmail.com>
2025-05-13 14:13:56 +0400

    Update лаба2.py

--- лаба2/лаба2.py
+++ лаба2/лаба2.py
@@ -33,3 +33,16 @@ if nums:
     print(f"Найдено: {len(nums)}\nЧисла: {nums}\nМаксимум прописью: {ntw(max(nums))}")
 else: print("Совпадений не найдено")
 input()
+
+
+import re
+def ntw(n): return ' '.join({'0':'ноль','1':'один','2':'два','3':'три','4':'четыре','5':'пять','6':'шесть','7':'семь','8':'восемь','9':'девять'}[d] for d in str(n))
+try: data = open("input.txt", encoding="utf-8").read()
+except: input("Ошибка открытия файла\nEnter для выхода..."); exit()
+pattern = r'\b[0-9a-fA-F]{8,}\b'
+matches = re.findall(pattern, data)
+nums = [int(m, 16) for m in matches if int(m, 16) <= 2047 and int(m, 16) % 2 == 0]
+if nums:
+    print(f"Найдено: {len(nums)}\nЧисла: {nums}\nМаксимум прописью: {ntw(max(nums))}")
+else: print("Совпадений не найдено")
+input()
